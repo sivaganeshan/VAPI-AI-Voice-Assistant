125624c35b92ed84c9075cae5751be049828c4dd
Tim <techwithtimm@gmail.com>
2025-02-06 17:59:09 +0400

    first commit

---
+++ frontend/src/call/AssistantSpeechIndicator.jsx
@@ -0,0 +1,16 @@
+const AssistantSpeechIndicator = ({ isSpeaking }) => {
+  return (
+    <div className="assistant-speech-editor">
+      <div
+        className={`speech-indicator ${
+          isSpeaking ? "speaking" : "not-speaking"
+        }`}
+      ></div>
+      <p className="speech-status">
+        {isSpeaking ? "Assistant Speaking" : "Assistant Not Speaking"}
+      </p>
+    </div>
+  );
+};
+
+export default AssistantSpeechIndicator
